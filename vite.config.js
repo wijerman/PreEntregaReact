d35d1ba6ed5936ac5ee4a40e18727ae90de71c77
wijerman <wijerman@gmail.com>
2024-06-02 18:02:54 -0300

    viteconfig final

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '/assets': '/src/assets',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['firebase/app'],
-    },
-  },
-});
+})
